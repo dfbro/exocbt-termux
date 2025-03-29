89a24e4028b0af0430a153a63ff855a3bf2b9d77
dfbro <robloslovers@gmail.com>
2025-03-29 18:11:34 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+## instalasi dependensi: qemu-x86_64, unzip, wget dan postgresql
+echo "instalasi dependensi"
+apt update
+apt install -y qemu-user-x86-64 unzip postgresql wget
+
+clear
+## download exo cbt 4.5.0 dan inisialisasi folder
+echo "Download Ekstraordinary"
+wget https://s3.ekstraordinary.com/extraordinarycbt/release-rosetta/4.5.0-linux.zip -O cbt.zip
+unzip cbt.zip -d ./cbt
+rm cbt.zip
+
+clear
+## inisialisasi postgresql
+echo "Inisialisasi Database"
+mkdir -p ./cbt/database
+initdb ./cbt/database
+echo "host all postgres 0.0.0.0/0 md5" >> ./cbt/database/pg_hba.conf
+pg_ctl -D ./cbt/database -l ./cbt/database.log start
+createuser -s postgres
+psql -U postgres -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+psql -U postgres -c "CREATE DATABASE exo;"
+psql -U postgres -d exo -f ./cbt/exo-dump-master.sql
+pg_ctl -D ./cbt/database stop
+
+clear
+## sentuhan terakhir
+sed -i 's|^STORAGE_PATH=.*|STORAGE_PATH='"$PWD"'/cbt/storage|' ./cbt/.env
+wget
+chmod +x ./startexo
+echo "ketik ./startexo untuk menjalankan Ektraordinary CBT"
